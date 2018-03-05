4c0f07a53ee588f109fce8096c2ed59f0131f83a
scott <you@example.comitsmescottb123@gmail.com>
2018-03-05 16:43:38 -0500

    characters create and show pages

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,43 @@
 class CharactersController < ApplicationController
+
+  before_action :find_character, only: [:show, :edit, :destroy]
+  
+  def index
+    @characters = Character.all
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.create(character_params)
+    if @character.save
+      redirect_to @character
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_character
+    @character = Character.find(params[:id])
+  end
+
+  def character_params
+    params.require(:character).permit(:name, :height, :weight, :speed, :strength, :image_url)
+  end
 end
