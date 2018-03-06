eb2871c90fe7b1f1063e4f95a9dc4ed0af46545e
scott <you@example.comitsmescottb123@gmail.com>
2018-03-06 11:38:57 -0500

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-<<<<<<< HEAD
       redirect_to @user
-=======
-      redirect_to users_path
->>>>>>> refs/remotes/origin/master
     else
       render :new
     end
   end
 
   def show
-    # @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-  
+
   resources :users
   resources :characters
 
   root to: 'welcome#welcome'
-=======
-  root to: 'welcome#welcome'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  resources :users
-  resources :characters
->>>>>>> refs/remotes/origin/master
+
 end
