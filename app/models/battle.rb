d7b69f2ebf05c06a08e9e00a755865e9f7f28a31
scott <you@example.comitsmescottb123@gmail.com>
2018-03-07 15:19:06 -0500

    updated fight moves

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -13,41 +13,73 @@ class Battle < ApplicationRecord
     User.find(self.game.user_id)
   end
 
-  def player_move
-    if self.attack == 'punch'
-      2 * get_player_character.strength
-    elsif self.attack == 'kick'
-      get_player_character.strength + get_player_character.speed
-    elsif self.attack == 'block'
-      10
-    end
-  end
-
   def get_enemy_move
-    @move = ['punch', 'kick', 'block'].sample
+    @enemy_move = ['punch', 'kick', 'block'].sample
   end
 
-  def enemy_move
-    if @move == 'punch'
-      2 * get_enemy_character.strength
-    elsif @move == 'kick'
-      get_enemy_character.strength + get_enemy_character.speed
-    elsif @move == 'block'
-      10
+  def random_num
+    25 + rand(50)
+  end
+
+  def move
+    if self.attack == 'punch' && @enemy_move == 'kick'
+      get_player_character.strength + random_num
+    elsif self.attack == 'kick' && @enemy_move == 'punch'
+      -get_enemy_character.strength - random_num
+    elsif self.attack == 'block' && @enemy_move == 'kick'
+      -get_enemy_character.speed - random_num
+    elsif self.attack == 'block' && @enemy_move == 'punch'
+      get_player_character.weight / 5 + random_num
+    elsif self.attack == 'kick' && @enemy_move == 'block'
+      get_player_character.strength + random_num
+    elsif self.attack == 'punch' && @enemy_move == 'block'
+      get_enemy_character.weight / -5 - random_num
     end
   end
 
   def fight
-    if player_move < enemy_move
-      outcome = get_player_character.health - (enemy_move - player_move)
+    if move.to_i < 0
+      outcome = get_player_character.health + move
       get_player_character.update(health: outcome)
-    elsif player_move > enemy_move
-      outcome = get_enemy_character.health - (player_move - enemy_move)
+    elsif move.to_i > 0
+      outcome = get_enemy_character.health - move
       get_enemy_character.update(health: outcome)
-    elsif
+    else
       "its a tie"
     end
   end
+
+  # def player_move
+  #   if self.attack == 'punch'
+  #     2 * get_player_character.strength
+  #   elsif self.attack == 'kick'
+  #     get_player_character.strength + get_player_character.speed
+  #   elsif self.attack == 'block'
+  #     10
+  #   end
+  # end
+
+  # def enemy_move
+  #   if @move == 'punch'
+  #     2 * get_enemy_character.strength
+  #   elsif @move == 'kick'
+  #     get_enemy_character.strength + get_enemy_character.speed
+  #   elsif @move == 'block'
+  #     10
+  #   end
+  # end
+
+  # def fight
+  #   if player_move < enemy_move
+  #     outcome = get_player_character.health - (enemy_move - player_move)
+  #     get_player_character.update(health: outcome)
+  #   elsif player_move > enemy_move
+  #     outcome = get_enemy_character.health - (player_move - enemy_move)
+  #     get_enemy_character.update(health: outcome)
+  #   elsif
+  #     "its a tie"
+  #   end
+  # end
 
   def user_victory
     outcome = get_user.wins.to_i + 1
